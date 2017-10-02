9b93e7da43440ef24e26bd149123fa32ebdcbdeb
Geramy92 <admin@terratex.eu>
2017-10-02 20:26:38 +0200

    add blips

--- resources/TerraTex-RL-RPG/Lib/Enviroment/GeneralBlips.cs
+++ resources/TerraTex-RL-RPG/Lib/Enviroment/GeneralBlips.cs
@@ -8,13 +8,20 @@ namespace TerraTex_RL_RPG.Lib.Enviroment
     {
         public GeneralBlips()
         {
+            API.onResourceStart += CreateBlips;
+        }
+
+        private void CreateBlips()
+        {
+            API.consoleOutput("Create general Information Blips");
+
             // Rookiespawn
             Blip rblip = API.createBlip(new Vector3(259.8162, -1204.156, 29.28907));
             rblip.color = 83;
             rblip.name = "Rookiespawn";
             rblip.sprite = 480;
             rblip.shortRange = true;
-          
+
             // City Hall
             Blip cHBlip = API.createBlip(new Vector3(245, -381.8, 44.5));
             cHBlip.color = 83;
