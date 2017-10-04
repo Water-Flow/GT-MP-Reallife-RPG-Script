9778ae3918ddda98f586af00917c1119ad31cf4e
Geramy92 <admin@terratex.eu>
2017-10-04 21:30:33 +0200

    #42 add json generation for menu creation

---
+++ resources/TerraTex-RL-RPG/Lib/LicenseSystem/LicenseMenu.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Shared;
+using GrandTheftMultiplayer.Shared.Math;
+using Newtonsoft.Json.Linq;
+
+namespace TerraTex_RL_RPG.Lib.LicenseSystem
+{
+    public class LicenseMenu : Script
+    {
+        public LicenseMenu()
+        {
+            API.onResourceStart += CreateMenuMarker;
+        }
+
+        private void CreateMenuMarker()
+        {
+            API.createMarker(29, new Vector3(243.17, -408.3, 48), new Vector3(0, 0, 0), new Vector3(0, 0, 0),
+                new Vector3(1, 1, 1), 255, 0, 255, 255, 0);
+            ColShape shape = API.createCylinderColShape(new Vector3(243.17, -408.3, 48), 1, 3);
+            shape.setData("isLicenseMarker", true);
+
+
+            API.onEntityEnterColShape += OnEntityEnterColShapeHandler;
+        }
+
+        private void OnEntityEnterColShapeHandler(ColShape colshape, NetHandle entity)
+        {
+            if (API.getEntityType(entity) == EntityType.Player)
+            {
+                if (colshape.hasData("isLicenseMarker"))
+                {
+                    Client player = API.getPlayerFromHandle(entity);
+
+                    // @todo: create Menu
+                    List<Dictionary<string, dynamic>> vehicleLicenseList = new List<Dictionary<string, dynamic>>();
+                    List<Dictionary<string, dynamic>> weaponLicenseList = new List<Dictionary<string, dynamic>>();
+                    List<Dictionary<string, dynamic>> featureLicenseList = new List<Dictionary<string, dynamic>>();
+
+                    List<string> ownedLicenses = new List<string>();
+                    List<ILicense> userLicenses = (List<ILicense>) player.getData("UserLicenses");
+
+                    foreach (ILicense lic in userLicenses)
+                    {
+                        ownedLicenses.Add(lic.GetLicenseIdentifierName());
+                    }
+
+                    foreach (ILicense lic in Licenses.GetAllLicenses())
+                    {
+                        Dictionary<string, dynamic> licenseObject = new Dictionary<string, dynamic>();
+                        licenseObject.Add("price", lic.GetLicensePrice());
+                        licenseObject.Add("identifier", lic.GetLicenseIdentifierName());
+                        licenseObject.Add("name", lic.GetHumanReadableName());
+                        licenseObject.Add("description", lic.GetHumanReadableDescription());
+                        if ((int) player.getSyncedData("Level") < lic.GetMinRequiredLevel())
+                        {
+                            licenseObject.Add("error", "Level " + lic.GetMinRequiredLevel());
+                            licenseObject.Add("enabled", false);
+                        }
+                        else if (ownedLicenses.Contains(lic.GetLicenseIdentifierName()))
+                        {
+                            licenseObject.Add("error", "im Besitz");
+                            licenseObject.Add("enabled", false);
+                        }
+                        else if ((float) player.getSyncedData("Money") < lic.GetLicensePrice())
+                        {
+                            licenseObject.Add("color", "~r~");
+                            licenseObject.Add("enabled", false);
+                        }
+                        else
+                        {
+                            licenseObject.Add("enabled", true);
+                        }
+
+                        var memberInfo = lic.GetType().BaseType;
+                        if (memberInfo != null && memberInfo.Name.Equals("VehicleLicense"))
+                        {
+                            vehicleLicenseList.Add(licenseObject);
+                        }
+                        else if (memberInfo != null && memberInfo.Name.Equals("FeatureLicense")) { 
+                            featureLicenseList.Add(licenseObject);
+                        }
+                        else if (memberInfo != null && memberInfo.Name.Equals("WeaponLicense"))
+                        {
+                            weaponLicenseList.Add(licenseObject);
+                        }
+                    }
+
+                    Dictionary<string, dynamic> licensesDictionary = new Dictionary<string, dynamic>();
+                    licensesDictionary.Add("vehicleLicenses", vehicleLicenseList);
+                    licensesDictionary.Add("featureLicenses", featureLicenseList);
+                    licensesDictionary.Add("weaponLicenses", weaponLicenseList);
+
+                    string json = JObject.FromObject(licensesDictionary).ToString();
+                    player.triggerEvent("createLicensesMenu", json);
+                }
+            }
+        }
+    }
+}
--- resources/TerraTex-RL-RPG/Lib/LicenseSystem/LicenseMenu.js
+++ resources/TerraTex-RL-RPG/Lib/LicenseSystem/LicenseMenu.js
@@ -1,21 +1,23 @@
-﻿// marker pos 239.0942, -409.409, 47.92436
-/*
-API.onResourceStart.connect(function () {
-
-    const mainMenu = API.createMenu("Lizensen", "Lizensarten:", 0, 0, 6);
-
-    const vehicleMenu = API.createMenu("Fahrzeuglizensen", "Kaufbare Lizensen:", 0, 0, 6);
-
-    for (var i = 0; i < 5; i++) {
-        vehicleMenu.AddItem(API.createDynamicListItem("label " + i, "labeldescription " + i, "test " + i));
-    }
-
-    API.addSubMenu(mainMenu, vehicleMenu, "Fahrzeuglizensen", "Lizensen zum führen verschiedener Fahrzeuge, Flugzeuge, ...");
-
-    vehicleMenu.OnItemSelect.connect(function (sender, item, index) {
-        API.sendChatMessage("You selected: ~g~" + item.Text);
-        
-    });
-
-    mainMenu.Visible = true;
-});*/
+﻿//API.onResourceStart.connect(function () {
+//
+//    const mainMenu = API.createMenu("Lizensen", "Lizensarten:", 0, 0, 6);
+//
+//    const vehicleMenu = API.createMenu("Fahrzeuglizensen", "Kaufbare Lizensen:", 0, 0, 6);
+//
+//    for (var i = 0; i < 5; i++) {
+//        const item = API.createMenuItem("label " + i, "labeldescription " + i);
+//        item.SetRightLabel("~r~test");
+//        item.Enabled = false;
+//        vehicleMenu.AddItem(item);
+//    }
+//
+//    API.addSubMenu(mainMenu, vehicleMenu, "Fahrzeug~r~lizensen", "Lizensen zum führen verschiedener Fahrzeuge, Flugzeuge, ...");
+//
+//    vehicleMenu.OnItemSelect.connect(function (sender, item, index) {
+//        API.sendChatMessage("You selected: ~g~" + item.Text);
+//        
+//    });
+//
+//    mainMenu.Visible = true;
+//});
+// @todo: createEvent: createLicensesMenu + updateLicensesMenu (-> updateLicensesMenu also server side after buying something)
