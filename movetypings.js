d95e261492c008d943139eb663f7b29c4aea0175
Geramy92 <admin@terratex.eu>
2017-10-15 01:36:34 +0200

    try to fix pipeline

---
+++ movetypings.js
@@ -0,0 +1,10 @@
+// this script is for our pipeline to find type files
+
+const fs = require('fs-extra');
+
+var glob = require("glob");
+
+// options is optional
+glob("**/types-gt-mp/", {}, function (er, files) {
+	fs.copySync(files[0], '/resources/TerraTex-RL-RPG/types-gt-mp')
+});
