cfa97e54384765358935f2ab07a1625f058f91fe
Geramy92 <admin@terratex.eu>
2017-10-14 22:05:15 +0200

    test typescript in pipeline

---
+++ resources/TerraTex-RL-RPG/Lib/Helper/Client/Dates.ts
@@ -0,0 +1,12 @@
+﻿function getGermanDateTimeString(date : Date) {
+    let dateString = "";
+    dateString += `${date.getDay()}.${date.getMonth()}.${date.getFullYear()}`;
+    dateString += ` ${date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()}`;
+    dateString += `:${date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()}`;
+    return dateString;
+}
+
+function getDifferenceOfTwoDatesInDays(date1 : Date, date2 : Date) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    return Math.ceil(timeDiff / (1000 * 3600 * 24));
+}
