ae83654cab3254af04481fd3197e152c04a27b81
Colin Weber <admin@terratex.eu>
2017-10-17 15:53:12 +0200

    Add Playerhelpfer to get player from partial name

--- resources/TerraTex-RL-RPG/Lib/Helper/Player.cs
+++ resources/TerraTex-RL-RPG/Lib/Helper/Player.cs
@@ -1,20 +1,54 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using GrandTheftMultiplayer.Server.API;
 using GrandTheftMultiplayer.Server.Elements;
 
 namespace TerraTex_RL_RPG.Lib.Helper
 {
     public static class PlayerHelper
     {
-        public static Client GetPlayerFromId(int Id)
+        public static Client GetPlayerById(int id)
         {
             List<Client> clients = TTRPG.Api.getAllPlayers();
 
             foreach (Client player in clients)
             {
-                if (player.getSyncedData("ID") == Id)
+                if ((int) player.getSyncedData("ID") == id)
                 {
                     return player;
                 }
+            }
+
+            return null;
+        }
+
+        public static Client GetPlayerByPartialNickname(string nickname)
+        {
+            Client playerFromName = API.shared.getPlayerFromName(nickname);
+            if (playerFromName != null)
+            {
+                return playerFromName;
+            }
+
+            if (nickname == null || nickname.Length <= 3)
+            {
+                return null;
+            }
+
+            List<Client> foundClients = new List<Client>();
+            List<Client> clients = TTRPG.Api.getAllPlayers();
+
+            foreach (Client player in clients)
+            {
+                if (player.name != null && player.name.IndexOf(nickname) != -1)
+                {
+                    foundClients.Add(player);
+                }
+            }
+
+            if (foundClients.Count == 1)
+            {
+                return foundClients[0];
             }
 
             return null;
