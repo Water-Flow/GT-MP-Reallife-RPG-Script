80ee35db65eaf9e668a0b809609ba13b4780e854
Colin Weber <admin@terratex.eu>
2017-10-19 16:14:41 +0200

    #62 Adds /players cmd to console

---
+++ resources/TerraTex-RL-RPG/Lib/Admin/GeneralConsoleCommands.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using TerraTex_RL_RPG.Lib.Threads;
+
+namespace TerraTex_RL_RPG.Lib.Admin
+{
+    public class GeneralConsoleCommands : Script
+    {
+        public GeneralConsoleCommands()
+        {
+            ConsoleReader.OnConsoleMessageEvent += OnConsoleMessage;
+        }
+
+        private void OnConsoleMessage(string cmd, string[] infos)
+        {
+            switch (cmd)
+            {
+                case "players":
+                    PlayersCmd();
+                    break;
+            }
+        }
+
+        private void PlayersCmd()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Players Online ");
+            
+            int maxPlayers = API.getMaxPlayers();
+            List<Client> clients = API.getAllPlayers();
+
+            sb.Append(clients.Count + "/" + maxPlayers + ": ");
+
+            foreach (Client client in clients)
+            {
+                sb.Append(client.name);
+                if (clients.Last() != client)
+                {
+                    sb.Append(", ");
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
